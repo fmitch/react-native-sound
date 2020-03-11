dd0b550302362714ca9ee59b8cb90176e1ba4121
fmitch <20970221+fmitch@users.noreply.github.com>
2020-03-11 14:08:37 -0600

    Update RNSoundModule.java

--- android/src/main/java/com/zmxv/RNSound/RNSoundModule.java
+++ android/src/main/java/com/zmxv/RNSound/RNSoundModule.java
@@ -385,6 +385,18 @@ public class RNSoundModule extends ReactContextBaseJavaModule implements AudioMa
       player.setPlaybackParams(player.getPlaybackParams().setSpeed(speed));
     }
   }
+	
+  @ReactMethod
+  public void setPitch(final Double key, final Float pitch) {
+	  if (android.os.Build.VERSION.SDK_INT < 23) {
+		  Log.w("RNSoundModule", "setPitch ignored due to sdk limit");
+		  return;
+	  }
+	  MediaPlayer player = this.playerPool.get(key);
+	  if (player != null) {
+		  player.setPlaybackParams(player.getPlaybackParams().setPitch(pitch));
+	  }
+  }
 
   @ReactMethod
   public void setCurrentTime(final Double key, final Float sec) {
